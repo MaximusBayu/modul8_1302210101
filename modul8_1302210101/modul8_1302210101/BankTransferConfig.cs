73df260e92160e7131c6816cba705f958acaaed1
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-04-11 17:14:46 +0700

    apliaksi bank transfer dengan runtime configuration

---
+++ modul8_1302210101/modul8_1302210101/BankTransferConfig.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302210101
+{
+    internal class BankTransferConfig
+    {
+        public class set
+        {
+            public string lang { get; set; }
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+            public string methods { get; set; }
+            public string en { get; set; }
+            public string id { get; set; }
+
+
+            public set(string lang, int threshold, int low_fee, int high_fee, string methods, string en, string id)
+            {
+                this.lang = lang;
+                this.threshold = threshold;
+                this.low_fee = low_fee;
+                this.high_fee = high_fee;
+                this.methods = methods;
+                this.en = en;
+                this.id = id;
+            }
+
+            public class readWrite
+            {
+                public set config;
+                public const string txt = @"./BankTransferConfig.json";
+
+                private void SetDefault()
+                {
+                    config = new set("en", 25000000, 6500, 15000, "RTO", "yes", "ya");
+                }
+
+                private void NewConfigFile()
+                {
+                    JsonSerializerOptions baru = new JsonSerializerOptions()
+                    {
+                        WriteIndented = true
+                    };
+
+                    string jsonString = JsonSerializer.Serialize(config, baru);
+                    File.WriteAllText(txt, jsonString);
+                }
+
+                private set ReadConfigFile()
+                {
+                    string readJSON = File.ReadAllText(txt);
+                    config = JsonSerializer.Deserialize<set>(readJSON);
+                    return config;
+                }
+
+                public readWrite()
+                {
+                    try
+                    {
+                        ReadConfigFile();
+                    }
+                    catch (Exception)
+                    {
+                        SetDefault();
+                        NewConfigFile();
+                    }
+                }
+
+                public void pilihM(int i)
+                {
+                    if (i == 1)
+                    {
+                        config.methods = "RTO(real-time)";
+                    }
+                    else if (i == 2)
+                    {
+                        config.methods = "SKN";
+                    }
+                    else if (i == 3)
+                    {
+                        config.methods = "RTGS";
+                    }
+                    else if (i == 4)
+                    {
+                        config.methods = "BI FAST";
+                    }
+                }
+
+                public void Ubahlang()
+                {
+                    if (config.lang == "en")
+                    {
+                        config.lang = "id";
+                        NewConfigFile();
+                    }
+                    else
+                    {
+                        config.lang = "en";
+                        NewConfigFile();
+                    }
+                }
+
+            }
+        }
+    }
+}
--- modul8_1302210101/modul8_1302210101/Program.cs
+++ modul8_1302210101/modul8_1302210101/Program.cs
@@ -1,2 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static modul8_1302210101.BankTransferConfig.set;
+
+readWrite rw = new readWrite();
+
+rw.Ubahlang();
+
+if (rw.config.lang == "en")
+{
+    Console.WriteLine("Please insert the ammount money to transfer: ");
+}
+else if (rw.config.lang == "id") {
+    Console.WriteLine("Masukan Jumlah Uang yang akan di-transfer");
+}
+
+int uang = Convert.ToInt32(Console.ReadLine());
+
+int fee = 0;
+if (uang <= rw.config.threshold)
+{
+    fee = rw.config.low_fee;
+}
+else { 
+    fee = rw.config.high_fee;
+}
+Console.WriteLine(" ");
+if (rw.config.lang == "en")
+{
+    Console.WriteLine("Transfer fee = "+fee);
+    Console.WriteLine("Total amount = " + (uang+fee));
+}
+else if (rw.config.lang == "id")
+{
+    Console.WriteLine("Biaya transfer = " + fee);
+    Console.WriteLine("Total biaya = " + (uang + fee));
+}
+Console.WriteLine(" ");
+if (rw.config.lang == "en")
+{
+    Console.WriteLine("Select transfer method: ");
+}
+else if (rw.config.lang == "id")
+{
+    Console.WriteLine("Pilih metode transfer: ");
+}
+
+Console.WriteLine("1. RTO (real-time) ");
+Console.WriteLine("2. SKN ");
+Console.WriteLine("3. RTGS ");
+Console.WriteLine("4. BI FAST ");
+
+int in2 = Convert.ToInt32(Console.ReadLine());
+
+if (rw.config.lang == "en")
+{
+    Console.WriteLine("Please type " + in2 + " to confirm the transaction: ");
+}
+else if (rw.config.lang == "id")
+{
+    Console.WriteLine("Ketik " + in2 + " untuk mengkonfirmasi transaksi: ");
+}
+
+int acc = Convert.ToInt32(Console.ReadLine());
+rw.pilihM(acc);
+
+if (rw.config.lang == "en" && acc > 4 || acc < 1)
+{
+    Console.WriteLine("Transfer is cancelled ");
+}
+else if (rw.config.lang == "id" && acc > 4 || acc < 1)
+{
+    Console.WriteLine("Transfer dibatalkan ");
+}
+else 
+{
+    if (rw.config.lang == "en")
+    {
+        Console.WriteLine("The transfer is completed ");
+    }
+    else if (rw.config.lang == "id")
+    {
+        Console.WriteLine("Proses transfer berhasil ");
+    }
+}
+
